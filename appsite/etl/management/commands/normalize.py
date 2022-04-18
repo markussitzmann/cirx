714e55187110a9164eedb031be4b46c90c05f921
Markus Sitzmann <markus.sitzmann@gmail.com>
2022-04-18 20:17:33 +0200

    compound normalization first working version

--- appsite/etl/management/commands/normalize.py
+++ appsite/etl/management/commands/normalize.py
@@ -16,25 +16,14 @@ class Command(BaseCommand):
     help = 'register files and file properties'
 
     def handle(self, *args, **options):
-        logger.info("register")
+        logger.info("normalize")
         _normalize()
 
-
-# def _register():
-#     #file_collections = FileCollection.objects.all()
-#     file_collections = FileCollection.objects.filter(id=4)
-#     for file_collection in file_collections:
-#         processor = FileRegistry(file_collection)
-#         file_list: List[StructureFile] = processor.register_files()
-#         file: StructureFile
-#         for file in file_list:
-#             logger.info("found file : %s" % file)
-#             processor.register_file_records(file)
 
 
 def _normalize():
 
-    FileRegistry.normalize_structures(range(10, 15))
+    FileRegistry.normalize_structures(range(1, 100))
 
 
     logger.info("ENS LIST %s", Ens.List())
