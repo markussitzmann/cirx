7ce3f1919be6ffc806c8822971d1d76909c352ca
Markus Sitzmann <markus.sitzmann@gmail.com>
2023-07-24 01:03:00 +0200

    some project and template improvements

--- appsite/appsite/settings.py
+++ appsite/appsite/settings.py
@@ -186,9 +186,6 @@ MEDIA_URL = "/media/"
 
 #### BOOTSTRAP
 
-#https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"
-#https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"
-
 BOOTSTRAP5 = {
     "css_url": {
         "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
