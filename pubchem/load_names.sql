7c82e4d6811db07d5fd7aed5e1bd01e2bf9ff5bc
Markus Sitzmann <markus.sitzmann@gmail.com>
2024-03-10 18:30:22 +0100

    improved versions of db dump and copy

--- pubchem/load_names.sql
+++ pubchem/load_names.sql
@@ -1,11 +1,11 @@
 BEGIN;
 
-CREATE TABLE IF NOT EXISTS cir_pubchem_structure_name_cid(
-    id bigserial PRIMARY KEY,
-    name_id bigint NOT NULL,
-    cid bigint NOT NULL,
-    UNIQUE(name_id, cid)
-);
+-- CREATE TABLE IF NOT EXISTS cir_pubchem_structure_name_cid(
+--     id bigserial PRIMARY KEY,
+--     name_id bigint NOT NULL,
+--     cid bigint NOT NULL,
+--     UNIQUE(name_id, cid)
+-- );
 
 CREATE TEMP TABLE tmp_structure_name(
     id bigserial PRIMARY KEY,
--- pubchem_load_names.sh
+++ pubchem_load_names.sh
@@ -3,5 +3,6 @@ set -e
 
 source .env
 
+./psql -f /home/app/pubchem/create_pubchem_database_extensions.sql
 ./psql -f /home/app/pubchem/load_names.sql
 
