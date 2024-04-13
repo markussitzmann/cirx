45fed15ebf8c9b5d3c9e1508fb1cb235c7c7c6ff
Markus Sitzmann <markus.sitzmann@gmail.com>
2024-04-13 19:38:56 +0200

    Pubchem load script improvements

---
+++ docs/dumps.md
@@ -0,0 +1,7 @@
+# List of database dumps
+
+* full-2024-04-11: contains PubChem SIDs merged into cir_records and cir_record_names
+
+# List of Backups
+
+* pubchem-sid-records-added.psql.bin contains the full build with PubChem SIDS merged into cir_records
--- pubchem/add_pubchem_substance_records.sql
+++ pubchem/add_pubchem_substance_records.sql
@@ -36,11 +36,31 @@ BEGIN;
 -- FROM cir_record r JOIN cir_pubchem_sid_map m ON r.regid::int=m.cid AND r.release_id=1 ORDER BY cid,source_id
 -- ON CONFLICT DO NOTHING;
 
-INSERT INTO cir_structure_file_record_names(name_id, name_type_id, structure_file_record_id)
-SELECT name_id,
-    12 as name_type_id,
-    structure_file_record_id
-FROM cir_record WHERE release_id=2;
+-- **** insert PubChem SIDS into cir_record_names table
+-- INSERT INTO cir_structure_file_record_names(name_id, name_type_id, structure_file_record_id)
+-- SELECT name_id,
+--     12 as name_type_id,
+--     structure_file_record_id
+-- FROM cir_record WHERE release_id=2;
+
+-- **** insert PubChem SIDS into cir_record_names table !!! Check if Publisher 4 is Pubchem, this is used for preliminary set uo
+-- INSERT INTO cir_dataset(name, modified, added, publisher_id)
+-- SELECT source_id as name, now(), now(), 4 FROM cir_pubchem_sid_map group by source_id
+-- ON CONFLICT DO NOTHING;
+
+-- **** corrections from init stat
+-- DELETE FROM cir_dataset where name = 'DTP/NCI' and publisher_id = 4;
+-- DELETE FROM cir_dataset where name = 'ChEMBL' and publisher_id = 4;
+
+
+SELECT name, description, NULL as description,
+       NULL as href, 'public' as class, 'active' as status, 0 as version, NULL as released,
+       2023-11-01 as downloaded, now() as added, now() as modified,
+       id as dataset_id,
+       1 as parent_id, 4 as publisher
+FROM cir_dataset as d;
+
+
 
 COMMIT;
 
