168dbae10411546fc85bd49cbd53b68c89d933c2
Markus Sitzmann <markus.sitzmann@gmail.com>
2024-04-19 04:21:58 +0200

    wip

--- docs/dumps.md
+++ docs/dumps.md
@@ -4,4 +4,9 @@
 
 # List of Backups
 
-* pubchem-sid-records-added.psql.bin contains the full build with PubChem SIDS merged into cir_records
+* pubchem-sid-records-added.psql.bin contains the full build with PubChem SIDS merged into cir_structure_names
+
+
+# Broken names
+
+* select name_id,(select name from cir_structure_name where id=name_id), count(structure_id) from cir_structure_name_associations where affinity_class_id=1 group by name_id order by count desc limit 10000;
--- pubchem/add_pubchem_substance_records.sql
+++ pubchem/add_pubchem_substance_records.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
--- ***** update if from original build
+-- 1 ***** update if from original build
 --UPDATE cir_dataset_release SET downloaded='2023-11-01' WHERE id = 1;
 
 -- insert **** PubChem Compound manually
@@ -12,7 +12,7 @@ BEGIN;
 --         NULL, 'public', 'active', 0, NULL, '2023-11-01', now(), now(), 1, NULL, 4)
 -- ON CONFLICT DO NOTHING;
 --
--- ***** insert PubChem Substance manually
+-- 2 ***** insert PubChem Substance manually
 -- INSERT INTO cir_dataset_release
 -- VALUES (
 --         2,
@@ -21,12 +21,12 @@ BEGIN;
 --         NULL, 'public', 'active', 0, NULL, '2023-11-01', now(), now(), 1, NULL, 4)
 -- ON CONFLICT DO NOTHING;
 
--- ***** insert PubChem SIDs into the name table
+-- 3 ***** insert PubChem SIDs into the name table
 --INSERT into public.cir_structure_name(hash,name) SELECT md5(sid::text)::uuid,sid FROM cir_pubchem_sid_map
 --ON CONFLICT DO NOTHING;
 
 
--- ***** insert PubChem SIDs into the record tables (check if PubChem Compound is Release 1 and PubChem Substance Release 2
+-- 4 ***** insert PubChem SIDs into the record tables (check if PubChem Compound is Release 1 and PubChem Substance Release 2
 -- INSERT INTO cir_record(regid, version, added, modified, dataset_id, name_id, release_id, structure_file_record_id)
 -- SELECT m.sid as regid,
 --        1 as version,
@@ -36,31 +36,34 @@ BEGIN;
 -- FROM cir_record r JOIN cir_pubchem_sid_map m ON r.regid::int=m.cid AND r.release_id=1 ORDER BY cid,source_id
 -- ON CONFLICT DO NOTHING;
 
--- **** insert PubChem SIDS into cir_record_names table
--- INSERT INTO cir_structure_file_record_names(name_id, name_type_id, structure_file_record_id)
--- SELECT name_id,
---     12 as name_type_id,
---     structure_file_record_id
--- FROM cir_record WHERE release_id=2;
+-- 5 **** insert PubChem SIDS into cir_record_names table
+INSERT INTO cir_structure_file_record_names(name_id, name_type_id, structure_file_record_id)
+SELECT name_id,
+    12 as name_type_id,
+    structure_file_record_id
+FROM cir_record WHERE release_id=2;
 
--- **** insert PubChem SIDS into cir_record_names table !!! Check if Publisher 4 is Pubchem, this is used for preliminary set uo
+-- 6 **** insert PubChem SIDS into cir_record_names table !!! Check if Publisher 4 is Pubchem, this is used for preliminary set uo
 -- INSERT INTO cir_dataset(name, modified, added, publisher_id)
 -- SELECT source_id as name, now(), now(), 4 FROM cir_pubchem_sid_map group by source_id
 -- ON CONFLICT DO NOTHING;
 
--- **** corrections from init stat
+-- 7 **** corrections from init stat
 -- DELETE FROM cir_dataset where name = 'DTP/NCI' and publisher_id = 4;
 -- DELETE FROM cir_dataset where name = 'ChEMBL' and publisher_id = 4;
 
 
-SELECT name, description, NULL as description,
-       NULL as href, 'public' as class, 'active' as status, 0 as version, NULL as released,
-       2023-11-01 as downloaded, now() as added, now() as modified,
-       id as dataset_id,
-       1 as parent_id, 4 as publisher
-FROM cir_dataset as d;
+
 
 
 
 COMMIT;
+
+-- *** KEEP
+-- SELECT name, description, NULL as description,
+--        NULL as href, 'public' as class, 'active' as status, 0 as version, NULL as released,
+--        2023-11-01 as downloaded, now() as added, now() as modified,
+--        id as dataset_id,
+--        1 as parent_id, 4 as publisher
+-- FROM cir_dataset as d;
 
